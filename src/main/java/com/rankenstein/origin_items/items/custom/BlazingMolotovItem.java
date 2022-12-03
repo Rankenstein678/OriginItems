6b6c931c899c2fb47e389d9d187192de5329939e
Rankenstein678 <94004557+Rankenstein678@users.noreply.github.com>
2022-12-03 23:40:33 +0100

    Initial commit

--- src/main/java/com/rankenstein/origin_items/items/custom/BlazingMolotovItem.java
+++ src/main/java/com/rankenstein/origin_items/items/custom/BlazingMolotovItem.java
@@ -10,7 +10,6 @@ import net.minecraft.entity.projectile.thrown.PotionEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.stat.Stats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.BlockHitResult;
@@ -40,17 +39,16 @@ public class BlazingMolotovItem extends Item {
 
                 @Override
                 protected void onCollision(HitResult hitResult) {
-
                     HitResult.Type type = hitResult.getType();
                     if (type == HitResult.Type.ENTITY) {
                         Entity entity = ((EntityHitResult) hitResult).getEntity();
                         entity.setOnFireFor(10);
-                        lightArea(entity.getSteppingPos(),1);
+                        lightArea(entity.getSteppingPos(), 1);
 
-                        this.onEntityHit((EntityHitResult)hitResult);
+                        this.onEntityHit((EntityHitResult) hitResult);
                         this.world.emitGameEvent(GameEvent.PROJECTILE_LAND, hitResult.getPos(), GameEvent.Emitter.of(this, null));
                     } else if (type == HitResult.Type.BLOCK) {
-                        BlockHitResult blockHitResult = (BlockHitResult)hitResult;
+                        BlockHitResult blockHitResult = (BlockHitResult) hitResult;
                         this.onBlockHit(blockHitResult);
                         BlockPos blockPos = blockHitResult.getBlockPos();
                         this.world.emitGameEvent(GameEvent.PROJECTILE_LAND, blockPos, GameEvent.Emitter.of(this, this.world.getBlockState(blockPos)));
@@ -71,7 +69,6 @@ public class BlazingMolotovItem extends Item {
                     } else {
                         lightArea(pos, 0);
                     }
-
                 }
 
 
@@ -82,18 +79,17 @@ public class BlazingMolotovItem extends Item {
                             if (center.getSquaredDistance(pos) > RANGE) continue;
                             BlockState blockState = world.getBlockState(pos);
                             if (DESTROYS.contains(blockState.getMaterial())) {
-                                world.setBlockState(pos, Blocks.FIRE.getDefaultState(),
-                                        Block.NOTIFY_ALL | Block.REDRAW_ON_MAIN_THREAD);
+                                world.setBlockState(pos, Blocks.FIRE.getDefaultState(), Block.NOTIFY_ALL | Block.REDRAW_ON_MAIN_THREAD);
                             }
                         }
                     }
                 }
             };
             potionEntity.setItem(itemStack);
+            //Todo: change Velocity?
             potionEntity.setVelocity(user, user.getPitch(), user.getYaw(), -20.0f, 0.5f, 1.0f);
             world.spawnEntity(potionEntity);
         }
-        user.incrementStat(Stats.USED.getOrCreateStat(this));
         if (!user.getAbilities().creativeMode) {
             itemStack.decrement(1);
         }
